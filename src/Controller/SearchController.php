d5e9074e28e84a160c27472d963236b1e1708eaf
kan takamasa <taka.96kc@gmail.com>
2017-02-19 22:43:29 +0900

    cakephp3

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link      http://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+class SearchController extends AppController
+{
+
+    /**
+     * Displays a view
+     *
+     * @return void|\Cake\Network\Response
+     * @throws \Cake\Network\Exception\ForbiddenException When a directory traversal attempt.
+     * @throws \Cake\Network\Exception\NotFoundException When the view file could not
+     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
+     */
+    public function index()
+    {
+        $path = func_get_args();
+        //urlデータ取得
+        $urlData = "https://tabelog.com/osaka/A2701/A270401/27086999/";
+        $html = file_get_contents($urlData);
+        $html = mb_convert_encoding($html, mb_internal_encoding(), "auto" );
+        //上記の変数$htmlが日本語ページだった場合、マルチバイトエンコーディングを行って、
+        //単なるテキストデータとして、新たに$htmlという変数に上書きで代入します。
+ 
+        preg_match_all( "/大阪府/", $html, $serchData);
+        var_dump($serchData);
+        $count = count($path);
+//        if (!$count) {
+//            return $this->redirect('/');
+//        }
+        if (in_array('..', $path, true) || in_array('.', $path, true)) {
+            throw new ForbiddenException();
+        }
+        $page = $subpage = null;
+
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+        $this->set(compact('page', 'subpage'));
+
+        try {
+//            $this->render(implode('/', $path));
+        } catch (MissingTemplateException $e) {
+            if (Configure::read('debug')) {
+                throw $e;
+            }
+            throw new NotFoundException();
+        }
+    }
+}
